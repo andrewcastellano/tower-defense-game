10e5ed044859c9396dde9730114d6ba87d41c355
Ben Jones <benjones@berkeley.edu>
2019-07-07 18:21:09 -0700

    remove custom css

--- in-game.php
+++ in-game.php
@@ -7,8 +7,6 @@
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
     <!-- Bootstrap -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <!-- Custom Style -->
-    <link href="/css/style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
     <script>
