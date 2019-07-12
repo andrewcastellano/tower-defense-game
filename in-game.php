1f81722deeb83e9f1fa552724a4fe29e256696d5
Andrew Castellano <andrew.castellano49@gmail.com>
2019-07-11 23:06:02 -0700

    Adding class definition for waterhose (level 1 tower)

--- in-game.php
+++ in-game.php
@@ -38,6 +38,30 @@
         level: {},
         player: {}
     };
+    
+    // Waterhose class definition
+    var waterhose = new Phaser.Class({ 
+        Extends: Phaser.GameObjects.Image,
+        initialize: function Waterhose(scene) {
+            Phaser.GameObjects.Image.call(this, scene, 0, 0, 'sprites', 'waterhose');
+            this.range = 0;
+            this.damage = 0;
+            this.pos = [0, 0];
+            this.cost = 25;
+        },
+        place: function() {
+            //definition for the place function
+        },
+        getEnemyInRange: function () {
+            //definition for finding enemy in range
+        },
+        fire: function() {
+            //definition for the fire function
+        },
+        update: function() {
+            //definition for the update function
+        }
+    });
 
     function preload ()
     {   
@@ -83,6 +107,9 @@
         this.add.text(765, 365, 'Save', { color: '#ffffff', fontSize: '12px' });
         this.add.image(850, 345, 'cancel').setScale(0.06);
         this.add.text(830, 365, 'Cancel', { color: '#ffffff', fontSize: '12px' });
+
+      
+        waterhoses = this.physics.add.group({ classType: waterhose, runChildUpdate: true });
     }
 
     function update ()
