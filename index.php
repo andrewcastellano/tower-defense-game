24227ea97c321b102c11d8985c89e172b1592edc
Andrew Castellano <andrew.castellano49@gmail.com>
2019-08-04 15:15:04 -0700

    updating file references

--- index.php
+++ index.php
@@ -9,7 +9,11 @@
     <script src="./src/enemies/Toaster.js"></script>
     <script src="./src/enemies/WashingMachine.js"></script>
     <script src="./src/towers/Waterhose.js"></script>
+    <script src="./src/towers/SignalDisruptor.js"></script>
+    <script src="./src/towers/Laser.js"></script>
     <script src="./src/projectiles/Waterdrop.js"></script>
+    <script src="./src/projectiles/Laserbeam.js"></script>
+    <script src="./src/projectiles/RadioWave.js"></script>
     <script src="./src/Gamestate.js"></script>
 </head>
 <body>
