497c45b401e8bc6e5269ad13d7ebcce2c4c1e76d
Ben Jones <benjones@berkeley.edu>
2019-08-04 18:20:02 -0700

    Add scenes for LoadGame, SaveGame, and TitlePage Enable buttons for loading and saving games Can click save button in Easy mode to save basic gamestate to local storage Saved data will appear as text in the loading screen

--- src/scenes/NewGame.js
+++ src/scenes/NewGame.js
@@ -2,20 +2,28 @@ class NewGame extends Phaser.Scene {
 	constructor() {
 		super({
 			key: 'NewGame',
-			active: true
+			active: false
 		})
 	}
 
 	create() {
-		const titleText = this.add.text(350, 185, 'AI UPRISING', { color: '#ff0000', fontSize: '48px' });
+		const titleText = this.add.text(300, 185, 'AI UPRISING', { color: '#ff0000', fontSize: '48px' });
 		const easyText = this.add.text(100, 100, 'EASY', { color: '#ffffff', fontSize: '24px' });
 		const mediumText = this.add.text(100, 200, 'MEDIUM', { color: '#ffffff', fontSize: '24px' });
 		const hardText = this.add.text(100, 300, 'HARD', { color: '#ffffff', fontSize: '24px' });
+		const cancelText = this.add.text(700, 200, 'CANCEL', { color: '#ffffff', fontSize: '24px'});
 
-		// Change scene to Easy game when text is clicked - maybe change to button in future
+
+		// Change scene to Easy
 		easyText.setInteractive();
 		easyText.on('pointerdown', () => {
 			this.scene.start('Easy');
+		});
+
+		// Change scene to TitlePage
+		cancelText.setInteractive();
+		cancelText.on('pointerdown', () => {
+			this.scene.start('TitlePage');
 		});
 	}
 }
