872b850c17547ffdc0dacccf870ec98dbc5c949a
Andrew Castellano <andrew.castellano49@gmail.com>
2019-08-14 21:20:15 -0700

    forgot this page

--- src/scenes/LoadGame.js
+++ src/scenes/LoadGame.js
@@ -7,17 +7,17 @@ class LoadGame extends Phaser.Scene {
 	}
 
 	create() {
-		const titleText = this.add.text(300, 185, 'AI UPRISING', { color: '#ff0000', fontSize: '48px' });
+		const titleText = this.add.text(300, 185, 'ai uprising', { color: '#ff0000', fontSize: '48px', fontFamily: 'Montserrat' });
 		const save1 = JSON.parse(localStorage.getItem('save1'));
 		const save1Text = save1
-			? this.add.text(100, 100, 
-				`Date: ${save1.date}\nScore: ${save1.score}\nLives: ${save1.lives}\nMoney: ${save1.money}`, 
-				{ color: '#ffffff', fontSize: '12px' })
-			: this.add.text(100, 100, 'SAVE 1', { color: '#ffffff', fontSize: '24px' });
+			? this.add.text(100, 100,
+				`date: ${save1.date}\nscore: ${save1.score}\nlives: ${save1.lives}\nmoney: ${save1.money}`,
+				{ color: '#ffffff', fontSize: '12px', fontFamily: 'Montserrat' })
+			: this.add.text(100, 100, 'save 1', { color: '#ffffff', fontSize: '24px', fontFamily: 'Montserrat' });
 		// const save1Text = this.add.text(100, 100, 'SAVE 1', { color: '#ffffff', fontSize: '24px' });
-		const save2Text = this.add.text(100, 200, 'SAVE 2', { color: '#ffffff', fontSize: '24px' });
-		const save3Text = this.add.text(100, 300, 'SAVE 3', { color: '#ffffff', fontSize: '24px' });
-		const cancelText = this.add.text(700, 200, 'CANCEL', { color: '#ffffff', fontSize: '24px'});
+		const save2Text = this.add.text(100, 200, 'save 2', { color: '#ffffff', fontSize: '24px', fontFamily: 'Montserrat' });
+		const save3Text = this.add.text(100, 300, 'save 3', { color: '#ffffff', fontSize: '24px', fontFamily: 'Montserrat' });
+		const cancelText = this.add.text(700, 200, 'cancel', { color: '#ffffff', fontSize: '24px', fontFamily: 'Montserrat' });
 
 		// var save1 = JSON.parse(localStorage.getItem('save1')) || 'save1 not found';
 		// console.log(save1);
