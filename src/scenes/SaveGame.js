1f1b4cd8206000834945889897d130db5cc9441d
Andrew Castellano <andrew.castellano49@gmail.com>
2019-08-14 21:22:25 -0700

    typography

--- src/scenes/SaveGame.js
+++ src/scenes/SaveGame.js
@@ -7,10 +7,10 @@ class SaveGame extends Phaser.Scene {
 	}
 
 	create() {
-		const titleText = this.add.text(300, 185, 'AI UPRISING', { color: '#ff0000', fontSize: '48px' });
-		const save1Text = this.add.text(100, 100, 'SAVE 1', { color: '#ffffff', fontSize: '24px' });
-		const save2Text = this.add.text(100, 200, 'SAVE 2', { color: '#ffffff', fontSize: '24px' });
-		const save3Text = this.add.text(100, 300, 'SAVE 3', { color: '#ffffff', fontSize: '24px' });
+		const titleText = this.add.text(300, 185, 'ai uprising', { color: '#ff0000', fontSize: '48px', fontFamily: 'Montserrat' });
+		const save1Text = this.add.text(100, 100, 'save 1', { color: '#ffffff', fontSize: '24px', fontFamily: 'Montserrat' });
+		const save2Text = this.add.text(100, 200, 'save 2', { color: '#ffffff', fontSize: '24px', fontFamily: 'Montserrat' });
+		const save3Text = this.add.text(100, 300, 'save 3', { color: '#ffffff', fontSize: '24px', fontFamily: 'Montserrat' });
 
 		save1Text.setInteractive();
 		save1Text.on('pointerdown', () => {
